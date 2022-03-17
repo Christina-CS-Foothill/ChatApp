a306e94b9aecaf1d27947ddb2c8c7855868f7f83
Christina-CS-Foothill <frjole16@gmail.com>
2022-03-17 12:41:15 -0700

    started adding image picking functionality

---
+++ lib/widgets/pickers/user_image_picker.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+
+class UserImagePicker extends StatefulWidget {
+  final void Function(File pickedImage) imagePickFn;
+
+  const UserImagePicker({Key? key, required this.imagePickFn})
+      : super(key: key);
+
+  @override
+  State<UserImagePicker> createState() => _UserImagePickerState();
+}
+
+class _UserImagePickerState extends State<UserImagePicker> {
+  File? _pickedImage = null;
+
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final pickedImageFile = await picker.getImage(
+      source: ImageSource.camera,
+    );
+    if (pickedImageFile == null) {
+      return;
+    }
+    setState(() {
+      _pickedImage = File(pickedImageFile.path);
+    });
+    widget.imagePickFn(_pickedImage!);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 40,
+          backgroundColor: Colors.grey,
+          backgroundImage:
+              _pickedImage != null ? FileImage(_pickedImage!) : null,
+        ),
+        FlatButton.icon(
+          textColor: Theme.of(context).primaryColor,
+          onPressed: _pickImage,
+          icon: Icon(Icons.image),
+          label: Text('Add Image'),
+        ),
+      ],
+    );
+  }
+}
